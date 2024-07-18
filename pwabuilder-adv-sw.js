7aafd649d79dbd0594f7eb780911107ed634628f
Ahmet Asaad Hammoud <a.h.ahmet.es@gmail.com>
2024-07-18 16:40:43 +0200

    done

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"3551f65978a87166e2744cd4f904fb47","url":"index.html"},{"revision":"05126073974c91cef9c16e908fbe1c09","url":"script.js"},{"revision":"d5059f032129e9ced3945a44b1a0f996","url":"service-worker.js"},{"revision":"97756e07e76b7a1c1f170dc5f898d9e9","url":"style.css"}]);
